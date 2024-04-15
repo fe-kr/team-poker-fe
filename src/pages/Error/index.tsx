cf035555e9ccb0728d2c031bf3b9f639bac0aaf3
fe-kr <max.kryshnev.fe.dev@gmail.com>
2024-04-15 03:23:15 +0300

    feat: add error page

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { NavLink, useRouteError } from 'react-router-dom';
+import { Button } from '@components';
+import { HistoryPaths } from '@constants/history';
+import { Container, ErrorMessage, Header } from './styles';
 
 const ErrorPage = () => {
-  return <>ErrorPage</>;
+  const { statusText, message } = useRouteError();
+
+  return (
+    <Container>
+      <Header>Oops!</Header>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <ErrorMessage>{statusText || message}</ErrorMessage>
+      <Button as={NavLink} to={HistoryPaths.home.path}>
+        Go To Home
+      </Button>
+    </Container>
+  );
 };
 
 export default ErrorPage;
---
+++ src/pages/Error/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+`;
+
+export const Header = styled.h1`
+  font-size: 4rem;
+  margin: 0 auto;
+`;
+
+export const ErrorMessage = styled.span`
+  margin-bottom: 1rem;
+`;
